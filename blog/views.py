bcf5e3f58b65f6e682c3d3c904e5cc22c8e3bda1
Shubham Pal <pals7363@gmail.com>
2019-10-16 21:34:00 +0530

    Post delete and save draft

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
 # Create your views here.
 def post_list(request):
-    posts=Post.objects.all().order_by('-published_date')
+    posts=Post.objects.filter(published_date__isnull=False).order_by('-published_date')
     return render(request,'blog/post_list.html',{'posts':posts})
 
 def post_detail(request,pk):
@@ -20,7 +20,7 @@ def post_new(request):
         if form.is_valid():
             post=form.save(commit=False)
             post.author =request.user 
-            post.published_date=timezone.now()
+           # post.published_date=timezone.now()
             post.save()
             return redirect('post_list')
     else:
@@ -36,7 +36,7 @@ def post_edit(request,pk):
         if form.is_valid():
             post=form.save(commit=False)
             post.author =request.user 
-            post.published_date=timezone.now()
+           # post.published_date=timezone.now()
             post.save()
             return redirect('post_detail',pk=post.pk)
     else:
@@ -44,4 +44,21 @@ def post_edit(request,pk):
 
 
     return render(request,'blog/post_edit.html',{'form':form})
+
+def post_draft_list(request):
+    posts=Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request,'blog/post_draft_list.html',{'posts':posts})
+
+def post_publish(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+    post.publish()
+    return redirect('post_detail',pk=pk) 
+
+def post_remove(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+
+
 
